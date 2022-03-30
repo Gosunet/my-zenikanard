fae97aa6d4c8e044fabd3fee3ea9451e43fa7dcc
Jonathan Lagneaux <lagneaux.j@gmail.com>
2022-03-30 17:22:53 +0200

    feat(app): extends loader to ipfs upload

--- packages/app/src/components/Mint.tsx
+++ packages/app/src/components/Mint.tsx
@@ -40,26 +40,31 @@ export const Mint: FC<MintProps> = ({ svgRef, setIsLoading }) => {
   const setupEventListener = async () => {
     // Most of this looks the same as our function askContractToMintNft
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (ethereum) {
         // Same stuff again
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        )
 
         // THIS IS THE MAGIC SAUCE.
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
-        connectedContract.on("NewNFTMinted", (from, tokenId) => {
+        connectedContract.on('NewNFTMinted', (from, tokenId) => {
           console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
-        });
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          )
+        })
 
-        console.log("Setup event listener!")
-
+        console.log('Setup event listener!')
       } else {
-        console.log("Ethereum object doesn't exist!");
+        console.log("Ethereum object doesn't exist!")
       }
     } catch (error) {
       console.log(error)
@@ -70,6 +75,7 @@ export const Mint: FC<MintProps> = ({ svgRef, setIsLoading }) => {
     console.log('Minting NFT')
 
     try {
+      setIsLoading(true)
       if (!svgRef.current) {
         throw new Error('No SVG')
       }
@@ -85,33 +91,31 @@ export const Mint: FC<MintProps> = ({ svgRef, setIsLoading }) => {
 
       const { ethereum } = window
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const connectedContract = new ethers.Contract(
-          CONTRACT_ADDRESS,
-          myEpicNft.abi,
-          signer
-        )
-
-        console.log('Going to pop wallet now to pay gas...')
-        setIsLoading(true)
-        let nftTxn = await connectedContract.makeAnEpicNFT(`ipfs://${cid}`)
-
-        console.log('Mining...please wait.')
-        await nftTxn.wait()
-
-        console.log(
-          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
-        )
-        console.log(
-          `https://testnets.opensea.io/collection/cryptoduck-kdjlvzgc9c`
-        )
-        setIsLoading(false)
-      } else {
-        setIsLoading(false)
-        console.log("Ethereum object doesn't exist!")
+      if (!ethereum) {
+        throw new Error('No ethereum provider')
       }
+
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      const connectedContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicNft.abi,
+        signer
+      )
+
+      console.log('Going to pop wallet now to pay gas...')
+      let nftTxn = await connectedContract.makeAnEpicNFT(`ipfs://${cid}`)
+
+      console.log('Mining...please wait.')
+      await nftTxn.wait()
+
+      console.log(
+        `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+      )
+      console.log(
+        `https://testnets.opensea.io/collection/cryptoduck-kdjlvzgc9c`
+      )
+      setIsLoading(false)
     } catch (error) {
       setIsLoading(false)
       console.log(error)
@@ -119,7 +123,7 @@ export const Mint: FC<MintProps> = ({ svgRef, setIsLoading }) => {
   }
 
   useEffect(() => {
-    setupEventListener();
+    setupEventListener()
   }, [])
 
   return (
